40bcca6d53285d0f52c7da7a376223dc37b7aacc
boonleng <boonleng@ou.edu>
2024-07-02 10:51:37 -0500

    Make things prettier

--- src/radar/cosmetics.py
+++ src/radar/cosmetics.py
@@ -90,20 +90,17 @@ class NumpyPrettyPrinter(pprint.PrettyPrinter):
                 array.data,
                 separator=", ",
                 suppress_small=True,
-                formatter={"float_kind": lambda x: "   ---" if x == array.fill_value else f"{x:6.2f}"},
+                formatter={"float_kind": lambda x: " -----" if x == array.fill_value else f"{x:6.2f}"},
             )
             mask_str = np.array2string(
                 array.mask,
                 separator=", ",
-                formatter={"bool_kind": lambda x: x},
+                formatter={"bool": lambda x: "  True" if x else " False"},
             )
             prefix = "array_2d("
-            subprefix = prefix + "data="
-            indented_lines = self.indent_lines(data_str, subprefix)
-            indented_lines += ",\n"
-            indented_lines += " " * self._current_indent
-            subprefix = " " * len(prefix) + "mask="
-            indented_lines += self.indent_lines(mask_str, subprefix, array.data.dtype, array.fill_value)
+            indented_lines = self.indent_lines(data_str, prefix + "data=")
+            indented_lines += ",\n" + " " * self._current_indent
+            indented_lines += self.indent_lines(mask_str, " " * len(prefix) + "mask=", array.data.dtype, array.fill_value)
         else:
             array_str = np.array2string(array, separator=", ", suppress_small=True, formatter={"float_kind": lambda x: f"{x:6.2f}"})
             indented_lines = self.indent_lines(array_str, "array_2d(", array.dtype)
