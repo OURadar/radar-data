792c0f307715d19baa133d34a48c81e46969946b
boonleng <boonleng@ou.edu>
2024-07-10 21:02:25 -0500

    Make it float instead and added FractionalTime

--- src/radar/read.py
+++ src/radar/read.py
@@ -325,7 +325,7 @@ def _read_wds_from_nc(ncid):
     return {
         "kind": Kind.WDS,
         "txrx": TxRx.MONOSTATIC,
-        "time": int(ncid.getncattr("Time")),
+        "time": float(ncid.getncattr("Time")) + ncid.getncattr("FractionalTime"),
         "latitude": float(ncid.getncattr("Latitude")),
         "longitude": float(ncid.getncattr("Longitude")),
         "sweepElevation": ncid.getncattr("Elevation") if "Elevation" in attrs else 0.0,
