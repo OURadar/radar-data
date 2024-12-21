7eee88a7d37ff3ff71f16cbd0d32b457909df500
boonleng <boonleng@ou.edu>
2024-12-21 13:55:27 -0600

    Expose read_tarinfo

--- src/radar/__init__.py
+++ src/radar/__init__.py
@@ -1,9 +1,9 @@
 __version__ = "1.0.0.dev2"
 
 from .common import *
-from .read import read
 from .fifobuffer import FIFOBuffer
 from .cosmetics import logFormatter
+from .read import read, read_tarinfo
 
 from . import cosmetics
 from . import product
