51efe607a2b4565dbd84474cb3fa70c70e02577a
Boonleng Cheong <boonleng@ou.edu>
2025-06-02 06:34:17 -0500

    Added a unit test (#6)
    * Reordered pyproject.toml like setuptools
    
    * Adding some unit tests
    
    * Added M postfix to the stripped file pattern
    
    * Updated test file list
    
    * Minor version bump
    
    * Updated README.md

--- src/radar/common.py
+++ src/radar/common.py
@@ -7,12 +7,12 @@ re_2parts = re.compile(
 re_3parts = re.compile(
     r"(?P<name>.+)[._-]"
     + r"(?P<time>20\d{2}(0\d|1[012])([012]\d|3[01])[._-]([01]\d|2[0-3])[0-5]\d[0-5]\d)[._-]"
-    + r"(?P<scan>([EA]\d+\.\d|N\d+))"
+    + r"(?P<scan>([EAN]\d+\.\d|N\d+))"
 )
 re_4parts = re.compile(
     r"(?P<name>.+)[._-]"
     + r"(?P<time>20\d{2}(0\d|1[012])([012]\d|3[01])[._-]([01]\d|2[0-3])[0-5]\d[0-5]\d)[._-]"
-    + r"(?P<scan>([EA]\d+\.\d|N\d+))[._-]"
+    + r"(?P<scan>([EAN]\d+\.\d|N\d+))[._-]"
     + r"(?P<symbol>[A-Za-z0-9]+)"
 )
 re_cf_version = re.compile(r"(CF|Cf|cf).+-(?P<version>[0-9]+\.[0-9]+)")
